648ddf288880b0ff51fd0c04fe95507e840a0383
Manish Tomar <manish.tomar@rackspace.com>
2014-03-23 22:02:46 -0700

    history request is de-duped

--- restcli.py
+++ restcli.py
@@ -136,7 +136,6 @@ class RestCLI(object):
         if args.print_body:
             print(method.upper(), uri, '\n{}'.format(body) if body else '')
         # Store request in history
-        # TODO: De-dup request - store only if it is different from previous
         self.history.store_item(method.upper(), res_arg, body)
         # Send request
         r = requests.request(method.lower(), uri, data=body, headers=self.headers)
@@ -169,35 +168,45 @@ class History(object):
         except IOError:
             return 0
 
+    def _fname(self, index):
+        return '{:0=5d}'.format(index)
+
+    def _fpath(self, index):
+        return os.path.join(self.path, self._fname(index))
+
     def _extract_item(self, fpath, index=None, include_body=True):
         with open(fpath) as f:
             lines = f.readlines()
-            body = reduce(add, lines[2:], '') if include_body else None
+            body = reduce(add, lines[2:], '') or None if include_body else None
             return HistoryItem(lines[0].strip(), lines[1].strip(), body=body, index=index)
 
     def items(self, include_body=True):
         try:
             last = self._last()
             for findex in xrange(last, 0, -1):
-                yield self._extract_item(os.path.join(self.path, '{:0=5d}'.format(findex)),
-                                         index=(last - findex + 1), include_body=include_body)
+                yield self._extract_item(self._fpath(findex), index=(last - findex + 1),
+                                         include_body=include_body)
         except IOError:
             # Stop processing on any error
             pass
 
     def __getitem__(self, index):
-        findex = self._last() + 1 - index
-        return self._extract_item(os.path.join(self.path, '{:0=5d}'.format(findex)),
+        return self._extract_item(self._fpath(self._last() + 1 - index),
                                   include_body=True)
 
     def store_item(self, method, resource, body):
         # TODO: Should it take `HistoryItem` as arg?
-        new_filename = '{:0=5d}'.format(self._last() + 1)
-        with open(os.path.join(self.path, new_filename), 'w') as f:
+        last = self._last()
+        # Do not store if it is same as last item
+        last_item = self._extract_item(self._fpath(last))
+        if last_item == HistoryItem(method, resource, body=body):
+            return
+        new_filepath = self._fpath(last + 1)
+        with open(new_filepath, 'w') as f:
             args = body and (method, resource, body) or (method, resource, )
             print(*args, sep='\n', file=f)
             with open(os.path.join(self.path, 'HEAD'), 'w') as hf:
-                hf.write(new_filename)
+                hf.write(self._fname(last + 1))
 
 
 class HistoryItem(object):
@@ -212,6 +221,9 @@ class HistoryItem(object):
 
     def printable(self):
         return '{:<6}{:<10}{}'.format(self.index, self.method, self.resource)
+
+    def __eq__(self, other):
+        return isinstance(other, HistoryItem) and self.__dict__ == other.__dict__
 
 
 def get_body(last_body, method, uri, headers, res, replace, edit, tmpfile):
